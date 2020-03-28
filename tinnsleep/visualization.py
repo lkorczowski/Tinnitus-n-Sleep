e5dc6515b02946bc299fa6a73aed255e6fb91f60
Louis Korczowski <louis.korczowski@gmail.com>
2020-03-28 18:23:08 +0100

    enhancement of create_visual

--- tinnsleep/visualization.py
+++ tinnsleep/visualization.py
@@ -2,32 +2,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
-
-def create_visual(RAW, detect, duration, interval):
+def create_visual(raw, detect, duration, interval):
     """ Create two visuals of a burst detection : a simple plot enabling 
-    visualization of time repartition and a display over the real signal
+    visualization of time repartition and a display over the raw signal as annotations.
+    Note: any existing annotation of the raw instance will be removed.
     
     Parameters
     ----------
-    Raw: mne raw instance of the signal 
-    detect: ndarray, shape (n_trials)
-    duration : float, duration in seconds of an epoch
-    interval : float, interval between 2 epochs (enabling overlap)
+    raw : instance of mne.Raw
+            the signal of interest
+    detect: ndarray, shape (n_trials,)
+            the vector of labels for the annotation sampled at the interval rate
+    duration : float
+            duration in seconds of an epoch
+    interval : float
+            interval between two epochs enabling overlap (if duration == interval, no overlap)
     
     Returns
     -------
-    None
+    fig: instance of matplotlib.figure.Figure
+        Raw traces.
     """
-    fig = plt.figure()
-    plt.plot(detect)
-    
-    if len(RAW.annotations)>0:
-        RAW.annotations.delete(np.arange(0, len(RAW.annotations))) 
+    fig1 = plt.figure()
+    plt.plot(detect, marker='o', markeredgecolor='r')
+
+    if len(raw.annotations)>0:
+        raw.annotations.delete(np.arange(0, len(raw.annotations)))
     for i in range(len(detect)):
         if detect[i]:
-            RAW.annotations.append([interval * i], [duration], str("brux_burst"))
-    RAW.plot(scalings="auto")
-    
+            raw.annotations.append([interval * i], [duration], str("brux_burst"))
+    fig2 = raw.plot(scalings="auto")
             
-    return fig
+    return fig1, fig2
