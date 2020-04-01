e21679f8d9ce8a3f7e2fd2dc1408c821287ed290
Louis Korczowski <louis.korczowski@gmail.com>
2020-04-01 13:46:27 +0200

    multidimensional plot for timeseries

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,12 +1,24 @@
 import pytest
 import numpy as np
 from tinnsleep.visualization import plotTimeSeries
+import matplotlib.pyplot as plt
 
-@pytest.fixture
-def data():
+
+def test_plotTimeSeries():
     np.random.seed(42)
-    return np.random.randn(2, 400)
-
-
-def test_plotTimeSeries(data):
+    data = np.random.randn(400, 2)
     plotTimeSeries(data)
+    plt.show()
+
+
+def test_plotTimeSeries_1dim():
+    np.random.seed(42)
+    data = np.random.randn(10)
+    plotTimeSeries(data)
+
+
+def test_plotTimeSeries_incorrectdim():
+    np.random.seed(42)
+    data = np.random.randn(1, 2, 3)
+    with pytest.raises(ValueError, match="data should be two-dimensional"):
+        plotTimeSeries(data)
--- tinnsleep/visualization.py
+++ tinnsleep/visualization.py
@@ -1,29 +1,75 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plotTimeSeries(data, fig=None,
+
+def plotTimeSeries(data,
                    ch_names=None,
-                   sfreq=None,
+                   sfreq=1,
                    scalings=None,
-                   fontsize=12,
                    annotations=None,
                    **kwargs):
-    """Return the exact number of expected windows based on the samples (N), window_length (T) and interval (I)
+    """Advanced plotting of multidimensional time series from ndarray
 
     Parameters
     ----------
-    N: int
-        total number of samples of the full signal to be epoched
-    T: int
-        duration of each epoch in samples
-    I: int
-        interval between the start of each epoch (if T == I, no overlap), always > 0
+    data: array-line, shape (n_samples, n_dimension)
+        multidimensional time series
+    ch_names: list | iterable, shape (n_dimension,) | None
+        the labels for the time series
+    sfreq: float (default: 1)
+        sample rate (in Hz)
+    scalings: float | iterable, shape (n_dimension,)
+        value between two channels
+    annotations: a instance mne.Annotations | list of dictionary (default: {})
+        a list of annotation or dictionary containing the following fields:
+        {'onset': float (seconds), 'duration': float (seconds), 'description': str, orig_time': float (seconds)}
+        Example:
+        >>> # a list of one annotation starting after 0.5 second of duration 1.0 second named 'blink'
+        >>> annotations = [{'onset': 0.5, 'duration': 1.0, 'description': "blink", 'origin_time': 0.0}]
 
     Returns
     -------
     n_epochs: int
         estimated number of epochs
     """
+    shapeD = data.shape
+    if len(shapeD) == 1:
+        n_channels = 1
+        n_samples = shapeD[0]
+        data = np.expand_dims(data, axis=1)
+    elif len(shapeD) == 2:
+        n_channels = shapeD[1]
+        n_samples = shapeD[0]
+    elif len(shapeD) > 2:
+        raise ValueError("data should be two-dimensional")
 
-    plt.figure()
+    if ch_names is None:
+        ch_names = np.arange(0, n_channels)
+    elif isinstance(ch_names, str):
+        ch_names = [ch_names] * n_channels
+    elif isinstance(ch_names, (np.ndarray, list)):
+        if not len(ch_names) == n_channels:
+            raise ValueError('ch_names should be same length as the number of channels of data')
+
+
+    # remove median
+    data = data - np.median(data, axis=0)
+
+    if scalings is None:
+        # distance between two lines: maximum of the 95% percentile of each channel
+        scalings = np.max(np.quantile(np.abs(data), 0.975, axis=0))
+
+    # calculate multidimensional shifts based on scalings
+    shifts = np.linspace(0, 2 * scalings * (n_channels-1), n_channels)
+
+    # align timeseries with new offsets
+    data = data + shifts
+
+    times = np.linspace(0, (n_samples-1) / sfreq, num=n_samples)
+
+    # compute shift based on scalings
+
+    fig = plt.figure()
+    plt.plot(times, data, label=ch_names, **kwargs)
 
     return fig
