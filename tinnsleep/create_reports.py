749e96933df87669ed8bfec14ce3bdd2eecafb26
Louis Korczowski <louis.korczowski@gmail.com>
2020-05-07 13:48:54 +0200

    add preprocess2 as temporary

--- tinnsleep/create_reports.py
+++ tinnsleep/create_reports.py
@@ -80,6 +80,88 @@ def preprocess(raw, picks_chan, picks_imp, duration, interval, params, THR_imp=6
         return epochs, valid_labels
 
 
+def preprocess2(raw, duration, interval,
+                picks_chan="all",
+                is_good_kwargs=None,
+                filter_kwargs=None,
+                Thresholding_kwargs=None,
+                episode_kwargs=None,
+                merge_fun=np.all):
+    """Preprocesses raw and apply a list of operations for events detection. Each step can be muted by giving None.
+
+
+    raw -> picks_chan -> filter -> epoch --------> is_good ----------------------------> labels -+
+                                    |                                                            +--> merge labels-+
+                                    +--> Amplitude_Thresholding -> labels -> merge into episodes-+                 |
+                                    |                                                                              |
+                                   OUT                                                                            OUT
+    Parameters
+    ----------
+    raw: Instance of mne.Raw
+        the signal to analyze
+    duration : int
+        Number of elements (i.e. samples) on the epoch.
+    interval: int
+        Number of elements (i.e. samples) to move for the next epoch.
+    picks_chan: str | list | slice | None (default: "all")
+        Channels to include. Slices and lists of integers will be interpreted as channel indices. In lists, channel
+        type strings (e.g., ['meg', 'eeg']) will pick channels of those types, channel name strings
+        (e.g., ['MEG0111', 'MEG2623'] will pick the given channels. Can also be the string values “all” to pick all
+        channels, or “data” to pick data channels. None (default) will pick all data channels.
+    filter_kwargs : dict (default: None)
+        parameters for ``mne.raw.filter()`` to apply to subset picks_chan
+    is_good_kwargs : dict (default: None)
+        parameters for ``tinnsleep.signal.is_good_epochs()`` to apply to subset picks_chan
+    Thresholding_kwargs : dict (default: None)
+        parameters for ``tinnsleep.classification.AmplitudeThresholding`` to apply
+    episode_kwargs : dict (default: None)
+        parameters for ``tinnsleep.episode`` class to merge
+    merge_fun : function (default: numpy.all)
+        function to merge is_good and Amplitude_Thresholding (True: valid epoch, False: artifact detected)
+
+    Returns
+    -------
+    epochs : ndarray, shape (n_epochs, n_channels, duration)
+        Epoched view of `data`. Epochs are in the first dimension.
+    valid_labels : lsit of booleans
+        labels of the epochs as good (True) or bad (False) for future annotation and reporting
+    log : dictionary
+        logs of the preprocessing steps, including the number of epochs rejected at each step
+    """
+
+    if isinstance(filter_kwargs, dict):
+        raw = raw.filter(**filter_kwargs, picks=picks_chan)
+    elif filter_kwargs is None:
+        pass  # do nothing
+    else:
+        raise ValueError('`filter_kwargs` a dict of parameters to pass to ``mne.raw.filter`` or None')
+
+    epochs = RawToEpochs_sliding(raw, duration, interval, picks=picks_chan)
+
+    # Epoch rejection based on |min-max| thresholding
+    if is_good_kwargs is not None:
+        amplitude_labels, bad_lists = is_good_epochs(epochs, **is_good_kwargs)
+    else:
+        amplitude_labels, bad_lists = [True]*epochs.shape[0], []
+
+    suppressed_amp = np.sum(np.invert(amplitude_labels))
+
+    # Epoch rejection based on Root Mean Square Amplitude thresholding
+    if Thresholding_kwargs is not None:
+        X = rms(epochs)  # take only valid labels
+        pipeline = AmplitudeThresholding(**Thresholding_kwargs)
+        RMSlabels = pipeline.fit_predict(X)
+        suppressed_rms = np.sum(RMSlabels)
+
+    valid_labels = merge_fun(np.c_[np.invert(RMSlabels), amplitude_labels], axis=-1)
+    suppressed_all = np.sum(np.invert(valid_labels))
+    log = {"suppressed_rms_THR": suppressed_rms, "suppressed_amp_THR": suppressed_amp,
+                                     "suppressed_overall": suppressed_all, "total_nb_epochs": len(valid_labels)}
+
+    return epochs, valid_labels, log
+
+
+
 def reporting(epochs, valid_labels, THR_classif, time_interval, delim, n_adaptive=0, log={}, generate_report=generate_bruxism_report):
     """creates clinical reports of bruxism out of a epoch array, for different thresholding values
     Parameters
