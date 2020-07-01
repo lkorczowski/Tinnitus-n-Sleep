542a38b2c6584256e7f92d6fbc5a1d9650e05bd1
zeta Lenovo 1 <robin.guillard@gmail.com>
2020-07-01 09:31:35 +0200

    new results let's hope they are good

--- tinnsleep/config.py
+++ tinnsleep/config.py
@@ -10,7 +10,7 @@ class Config():
     """
 
     username = getpass.getuser()
-    known_users = {"Zeta": 'E:/Acou_sommeil/EDF_V2_PAUL',
+    known_users = {"Zeta": 'E:/Acou_sommeil/EDF_renom',
                     "louis": "/Users/louis/Data/SIOPI/bruxisme",
                     "runner": "/runner/"}
 
