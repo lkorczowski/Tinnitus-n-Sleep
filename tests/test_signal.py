263e8cc0b7f4760b2bdb2455cf3cbab76ebcb81e
Louis Korczowski <louis.korczowski@gmail.com>
2021-01-22 14:59:08 +0100

    latency alignment for different length epochs

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 import numpy.testing as npt
-from tinnsleep.signal import rms, is_good_epochs, _is_good_epoch, power_ratio
+from tinnsleep.signal import rms, is_good_epochs, _is_good_epoch, power_ratio, align_epochs_latency
 from tinnsleep.utils import epoch
 
 
@@ -77,3 +77,100 @@ def test_is_good_epoch_basic(data):
                                      flat_thresholds=flat_thresholds)
     npt.assert_equal(is_good, is_good_expected)
     npt.assert_equal(is_bad, report)
+
+
+def test_align_epochs_latency_dirac():
+    m_shape = (2, 25)
+    list_epochs = [np.zeros(m_shape), np.zeros(m_shape)]
+    list_epochs[0][1, 10] = 1
+    list_epochs[1][1, 20] = 1
+    npt.assert_equal(list_epochs[1][:, 20], list_epochs[0][:, 10])
+    list_argmax = []
+    for i, x in enumerate(list_epochs):
+        list_argmax.append(np.argmax(x))
+
+    list_shifts = min(list_argmax) - list_argmax
+    list_shifts = [5, -5]  # using both right hand and left hand padding
+    list_epochs = align_epochs_latency(list_epochs, list_shifts)
+
+    npt.assert_equal(list_epochs[1], list_epochs[0])
+
+
+def test_align_epochs_latency_dirac_different_length():
+    m_shape = (2, 25)
+    m_shape2 = (2, 30)
+    list_epochs = [np.zeros(m_shape), np.zeros(m_shape2)]
+    list_epochs[0][1, 10] = 1
+    list_epochs[1][1, 20] = 1
+    npt.assert_equal(list_epochs[1][:, 20], list_epochs[0][:, 10])
+    list_argmax = []
+    for i, x in enumerate(list_epochs):
+        list_argmax.append(np.argmax(x))
+
+    list_shifts = min(list_argmax) - list_argmax
+    list_shifts = [5, -5]  # using both right hand and left hand padding
+    list_epochs = align_epochs_latency(list_epochs, list_shifts)
+
+    npt.assert_equal(list_epochs[1][:, :m_shape[1]], list_epochs[0])
+
+
+def test_align_epochs_latency_dirac_multiple_left():
+    # calibrate all dirac of random latency to the earliest dirac
+    n_epochs = 10000
+    n_channels = 10
+    list_epochs = []
+    list_argmax = []
+    min_samples = 1000
+    max_samples = 0
+    for i in range(n_epochs):
+        n_samples = np.random.randint(25, 1000)
+
+
+        if n_samples<min_samples:
+            min_samples = n_samples
+        if n_samples > max_samples:
+            max_samples = n_samples
+
+        list_argmax.append(np.random.randint(10, n_samples))
+        list_epochs.append(np.zeros((n_channels, n_samples)))
+        list_epochs[-1][:, list_argmax[-1]] = 1
+
+    list_shifts = min(list_argmax) - np.array(list_argmax)
+
+    list_epochs_aligned = align_epochs_latency(list_epochs, list_shifts)
+
+    for i in range(len(list_epochs_aligned)):
+        npt.assert_equal(list_epochs_aligned[i][:, :min_samples], list_epochs_aligned[0][:, :min_samples])
+
+
+def test_align_epochs_latency_dirac_multiple_right():
+    # calibrate all dirac of random latency to the latest dirac
+    n_epochs = 10000
+    n_channels = 10
+    list_epochs = []
+    list_argmax = []
+    min_samples = 1000
+    max_samples = 0
+    for i in range(n_epochs):
+        n_samples = np.random.randint(25, 1000)
+
+
+        if n_samples<min_samples:
+            min_samples = n_samples
+        if n_samples > max_samples:
+            max_samples = n_samples
+
+        list_argmax.append(np.random.randint(0, n_samples))
+        list_epochs.append(np.zeros((n_channels, n_samples)))
+        list_epochs[-1][:, list_argmax[-1]] = 1
+
+
+    # calibrate all dirac of random latency to the latest dirac
+
+    list_shifts2 = max(list_argmax) - np.array(list_argmax)
+
+    list_epochs_aligned = align_epochs_latency(list_epochs, list_shifts2)
+
+    for i in range(len(list_epochs_aligned)):
+        npt.assert_equal(list_epochs[i][:, :max_samples], list_epochs[0][:,:max_samples])
+
